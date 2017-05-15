d6d506933289e28d2bd18c5ad526dfc46c1bf3b0
BK Box <bk.box@rackspace.com>
2017-05-15 20:08:03 +0000

    Add extra functionality to meet AC
    Signed-off-by: Matt Barlow <matt.barlow@rackspace.com>

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="splat",
+    version="1.0",
+    install_requires=[
+        "appdirs==1.4.3",
+        "docopt==0.6.2",
+        "packaging==16.8",
+        "pyparsing==2.2.0",
+        "six==1.10.0",
+        "systemd==0.9.15",
+    ],
+    python_requires=">=3.6",
+    packages=find_packages(),
+    scripts=['bin/splat'],
+)
---
+++ splat/splat.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3.6
+"""Splat Pomodoro Timer
+
+Usage:
+  splat start PTID PAIRS
+  splat status
+  splat pause PAIRS
+  splat (-h | --help)
+
+Arguments:
+  PTID		Pivotal Tracker ID
+  PAIRS		Comma separated list of initials
+
+Examples:
+  splat start 194492 bl,sk
+  splat start 194492 mb
+  splat pause bl,sk
+
+Options:
+  -h --help     Show this screen.
+"""
+
+from datetime import datetime, timedelta
+from docopt import docopt
+from systemd import journal
+import signal
+import os
+import sys
+import time
+import tempfile
+import glob
+
+REMAINING = 0
+PAUSED = False
+
+# This will cause tmp files to be cleaned up when PID is sent the kill signal.
+def receive_signal(signum, stack):
+    sys.exit()
+
+def pause(signum, stack):
+    global PAUSED
+    PAUSED = not PAUSED
+    if PAUSED:
+        print("Paused")
+    else:
+        print("Continuing")
+
+signal.signal(signal.SIGTERM, receive_signal)
+signal.signal(signal.SIGINT, receive_signal)
+signal.signal(signal.SIGUSR1, pause)
+
+
+def main(arguments):
+    if arguments['start']:
+        start(arguments)
+    elif arguments['status']:
+        status()
+    elif arguments['pause']:
+        send_pause(arguments)
+
+
+def send_pause(arguments):
+    pairs = set(arguments['PAIRS'].split(","))
+    try:
+        pid = get_pid_from_pairs(pairs)
+    except:
+        print("Session pairs not found.")
+        sys.exit()
+    os.kill(pid, signal.SIGUSR1)
+
+def start(arguments):
+    global PAUSED
+    PAUSED = False
+
+    try:
+        os.mkdir('/tmp/splat')
+    except FileExistsError:
+        pass
+
+    mypid = str(os.getpid()) + "\n"
+
+    fp = tempfile.NamedTemporaryFile(prefix="/tmp/splat/", suffix=".{}".format(str(os.getpid())))
+    fp.write(str.encode(str(datetime.now()) + "\n"))
+    fp.write(str.encode(mypid))
+    fp.write(str.encode(arguments['PAIRS'] + "\n"))
+    fp.flush()
+
+    journal.send(
+        message='Starting Pomodoro',
+        priority=journal.Priority.NOTICE,
+        PAIRS=f"{arguments['PAIRS']}",
+        PTID=f"{arguments['PTID']}",
+    )
+
+    global REMAINING
+
+    REMAINING = 25*60
+    while (REMAINING > 0):
+        if PAUSED:
+            time.sleep(5)
+            continue
+        fp.write(str.encode(str(REMAINING) + "\n"))
+        fp.flush()
+        time.sleep(5)
+        REMAINING -= 5
+        string_size = len(str.encode(str(REMAINING) + "\n"))
+        fp.seek(-string_size, 1)
+
+def status():
+    clean_stale_files()
+    temp_files = splat_tempfiles()
+    statusbar = ""
+    for f in temp_files:
+        tmpfile = open("/tmp/splat/" + f, 'r')
+        lines = tmpfile.readlines()
+        timestr = str(timedelta(seconds=int(lines[3].strip("\n"))))
+        statusbar += "{}: {}|".format(lines[2].strip("\n"), timestr)
+    if statusbar != "":
+        print("|" + statusbar)
+
+def splat_tempfiles():
+    return os.listdir("/tmp/splat")
+
+def clean_stale_files():
+    temp_files = splat_tempfiles()
+    for f in temp_files:
+        tmpfile = open("/tmp/splat/" + f, 'r')
+        lines = tmpfile.readlines()
+        creation_date = datetime.strptime(lines[0].strip("\n"), "%Y-%m-%d %H:%M:%S.%f")
+        if (datetime.now() - creation_date).seconds > 3000:
+            os.remove("/tmp/splat/" + f)
+
+def get_pid_from_pairs(pairs):
+    temp_files = splat_tempfiles()
+    for f in temp_files:
+        tmpfile = open("/tmp/splat/" + f, 'r')
+        lines = tmpfile.readlines()
+        pairs_from_file = set(lines[2].strip("\n").split(","))
+        if pairs_from_file == pairs:
+            return int(lines[1].strip("\n"))
+        else:
+            raise
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    main(arguments)
